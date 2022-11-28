da9b5a9fb0b4fc51e0d2cfac01e47bb2275a0a9d
Md Mijanur Rahman <md.rahman@fau.de>
2022-11-28 11:24:08 +0100

    comment added

--- Python Solution/217_Contains_Duplicate.py
+++ Python Solution/217_Contains_Duplicate.py
@@ -1,3 +1,11 @@
+'''
+Author: Md Mijanur Rahman
+Date: 28/11/2022
+Difficulty: Easy
+Problem Link: https://leetcode.com/problems/contains-duplicate/
+Problem Description: Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+'''
+
 from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
