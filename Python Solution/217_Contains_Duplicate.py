b168da4b617eb211a0f700a83c61238571bdba5c
Md Mijanur Rahman <md.rahman@fau.de>
2022-11-28 11:17:09 +0100

    217 added

---
+++ Python Solution/217_Contains_Duplicate.py
@@ -0,0 +1,19 @@
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        result_dict = {}
+        for num in nums:
+            if num in result_dict:
+                return True
+            else:
+                result_dict[num] = 0
+        return False
+
+if __name__ == "__main__":
+    nums_1 = [1,2,3,1] # True
+    nums_2 = [1,2,3,4] # False
+    nums_3 = [1,1,1,3,3,4,3,2,4,2] # True
+
+    solution = Solution()
+    for nums in [nums_1, nums_2, nums_3]:
+        print(solution.containsDuplicate(nums))
