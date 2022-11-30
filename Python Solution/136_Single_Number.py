9834e97e95f5a4c6d68bc77a0ea8271343194035
Md Mijanur Rahman <md.rahman@fau.de>
2022-11-30 08:42:47 +0100

    bit manipulation technique added

--- Python Solution/136_Single_Number.py
+++ Python Solution/136_Single_Number.py
@@ -10,15 +10,30 @@ You must implement a solution with a linear runtime complexity and use only cons
 from typing import List
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hashMap = {}
+        # hashMap = {}
+        # for num in nums:
+        #     if num not in hashMap:
+        #         hashMap[num] = 1
+        #     else:
+        #         hashMap[num] += 1
+        # for key, val in hashMap.items():
+        #     if val==1:
+        #         return key
+        """
+        Let's try another approach: bitwise XOR
+        If we take XOR of zero and some bit, it will return that bit. i.e. a⊕0=a
+        If we take XOR of two same bits, it will return 0. i.e. a⊕a=0
+        a⊕b⊕a=(a⊕a)⊕b=0⊕b=b
+        Now let's say we have a list [2,2,1]. If we store our result res=0 and then XOR it with each element in the list, we will get the following:
+        res=0⊕2=2
+        res=2⊕2=0
+        res=0⊕1=1
+        Elegenat, right? We can use this approach to solve the problem.
+        """
+        res = 0
         for num in nums:
-            if num not in hashMap:
-                hashMap[num] = 1
-            else:
-                hashMap[num] += 1
-        for key, val in hashMap.items():
-            if val==1:
-                return key
+            res ^= num
+        return res
 
 if __name__=='__main__':
     nums_1 = [2,2,1] # 1
@@ -27,3 +42,5 @@ if __name__=='__main__':
     solution = Solution()
     for nums in [nums_1, nums_2, nums_3]:
         print(solution.singleNumber(nums))
+
+        
