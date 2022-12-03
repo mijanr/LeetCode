44ec2105630584011da8a6e29f37a09ef21d27a2
Md Mijanur Rahman <md.rahman@fau.de>
2022-12-03 11:29:37 +0100

    sol added

---
+++ Python Solution/41_First_Missing_Positive.py
@@ -0,0 +1,31 @@
+"""
+Author: Md Mijanur Rahman
+Date: 03/12/2022
+Difficulty: Hard
+Problem Link: https://leetcode.com/problems/first-missing-positive/
+Problem Description: Given an unsorted integer array nums, return the smallest missing positive integer.
+
+You must implement an algorithm that runs in O(n) time and uses constant extra space.
+"""
+from typing import List
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        """
+        >>> Solution().firstMissingPositive([1,2,0])
+        3
+        >>> Solution().firstMissingPositive([3,4,-1,1])
+        2
+        >>> Solution().firstMissingPositive([7,8,9,11,12])
+        1
+        """
+        hashSet = set(nums)
+        ans = 1
+        while True:
+            if ans not in hashSet:
+                return ans
+            ans +=1 
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
