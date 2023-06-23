a73b694371514e54533f2b66acc9ead998c578ca
Md Mijanur Rahman <md.rahman@fau.de>
2023-06-23 18:33:12 +0200

    doctest added

--- Python Solution/704_Binary_Search.py
+++ Python Solution/704_Binary_Search.py
@@ -3,15 +3,23 @@ Author: Md Mijanur Rahman
 Date: 30/11/2022
 Difficulty: Easy
 Problem Link: https://leetcode.com/problems/binary-search/
-Problem Description: Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+Problem Description: Given an array of integers nums which is sorted in ascending order, and an integer target, 
+write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 
 You must write an algorithm with O(log n) runtime complexity.
 """
 from typing import List
+import doctest
 
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        """
+        >>>Solution().search([-1,0,3,5,9,12], 9)
+        4
+        >>>Solution().search([-1,0,3,5,9,12], 2)
+        -1
+        """
         first = 0
         final = len(nums)
         mid = len(nums) // 2
@@ -33,10 +41,12 @@ class Solution:
 
 
 if __name__ == "__main__":
-    nums_1 = [-1, 0, 3, 5, 9, 12]
+    """nums_1 = [-1, 0, 3, 5, 9, 12]
     target_1 = 9  # Output: 4
     nums_2 = [-1, 0, 3, 5, 9, 12]
     target_2 = 2  # Output: -1
     solution = Solution()
     print(solution.search(nums_1, target_1))
-    print(solution.search(nums_2, target_2))
+    print(solution.search(nums_2, target_2))"""
+
+    doctest.testmod(verbose=True)
