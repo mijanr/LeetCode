0b60e4e973651305f067c995f2ea47626ee4bacd
Md Mijanur Rahman <md.rahman@fau.de>
2023-12-10 12:33:21 +0100

    optimized the code with hashSet

--- PythonSolution/1213.py
+++ PythonSolution/1213.py
@@ -11,6 +11,7 @@ class Solution:
         >>> s.arraysIntersection([1,2,3,4,5], [1,2,5,7,9], [1,3,4,5,8,9])
         [1, 5]
         """
+        arr1, arr2 = set(arr1), set(arr2)
         return [num for num in arr1 if num in arr2 and num in arr3]
     
 if __name__ == "__main__":
